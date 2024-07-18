20a35b8eb991848048979dc0e3bad9d43be50298
dharun <dharundayaal@gmail.com>
2024-07-18 22:01:39 +0530

    learnt some bootstrap classes and added an nav bar.

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+function menuclick(){
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'flex';
+}
+
+function closeMenuBar(){
+    const closeSideBar = document.querySelector('.sidebar');
+    closeSideBar.style.display = 'none'
+}
